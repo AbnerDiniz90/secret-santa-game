de1cf441b6ff348adfc0a3fe7bc63478b63e11ef
Abner Diniz <abnerdiniz90@gmail.com>
2025-02-27 17:47:17 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let list = []
+let listRandom = []
+
+function adicionarAmigo(){
+    let input = document.querySelector('input').value;
+    input = input.trim();
+
+    if((input == "")){
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    if(list.includes(input)){
+        clearBuffer()
+        alert("Nome já inserido.")
+        return
+    }
+
+    list.push(input);
+    
+    clearBuffer()
+
+    exibirLista(list)
+}
+
+function clearBuffer(){
+    let inputCamp = document.querySelector('input');
+    inputCamp.value = "";
+}
+
+function exibirLista(list){
+    let displayList = document.getElementById('listaAmigos');
+    displayList.innerHTML = "";
+
+    for (let nome of list){ 
+        let li = document.createElement("li");
+        li.textContent = nome;
+
+        displayList.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    let listCopy = list.slice();
+    let n = listCopy.length;
+
+    if (listCopy.length == 0)
+        return
+    
+    while(listRandom.length < n){
+        let randomNumber = Math.floor(Math.random() * n);
+        
+        if(!(listRandom.includes(listCopy[randomNumber]))){
+            listRandom.push(listCopy[randomNumber]);
+
+            let displayName = document.getElementById('resultado');
+            displayName.innerHTML = `O amigo sorteado é: <strong>${listCopy[randomNumber]}</strong>`;
+
+            break;
+        }
+    }
+    console.log(listRandom)
+}
